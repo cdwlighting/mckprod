6a3e6723efb89a7114921ed7c12e8519e2048862
Chris Warner <cdw.lighting@gmail.com>
2013-05-22 21:51:47 -0400

    Fixed config file to include Modernizr.

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module McKelveyWarnerProductions
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.initialize_on_precompile = true
+    config.assets.precompile += ['modernizr.js', 'jquery.min.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
